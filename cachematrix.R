17f328f1c0056a441e53df1944a2ff81634878f2
Daniel Marjenburgh <dmarjenburgh@gmail.com>
2015-04-14 19:23:06 +0200

    implement cachematrix and cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This file contains 2 functions: makeCacheMatrix and cacheSolve. cacheSolve it
+## meant to be used on the return value of makeCacheMatrix. Please see their
+## descriptions below
 
-## Write a short comment describing this function
-
+## makeCacheMatrix creates an object that wraps a matrix and has methods to 
+## store and retrieve a precomputed cached value. The cacheMatrix supports any 
+## computed value, not just the inverse. Only a single value can be stored and
+## the cached value is overwritten the next time store is called. There is no
+## way to change the wrapped matrix value, instead it is better to create a new
+## cacheMatrix.
 makeCacheMatrix <- function(x = matrix()) {
-
+    cache <- NULL
+    get <- function() x
+    store <- function(f) {
+        cache <<- f(x)
+    }
+    getcache <- function() cache
+    list(get = get, store = store, getcache = getcache)
 }
 
-
-## Write a short comment describing this function
-
+## This function takes a cacheMatrix and any further arguments to be passed to 
+## solve and returns the inverse matrix. Makes use of the cacheMatrix' caching
+## capability
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    value <- x$getcache()
+    if (is.null(value)) {
+        # Compute inverse
+        x$store(function (x) {
+            solve(x, ...)
+        })
+    }
+    x$getcache()
 }
